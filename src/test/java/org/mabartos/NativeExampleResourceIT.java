d9ddf4e075919649e17155fbc4889d53a1ef3845
Martin Bartoš <mabartos@redhat.com>
2022-03-21 14:13:56 +0100

    Remove tests

--- src/test/java/org/mabartos/NativeExampleResourceIT.java
+++
@@ -1,9 +0,0 @@
-package org.mabartos;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeExampleResourceIT extends ExampleResourceTest {
-
-    // Execute the same tests but in native mode.
-}
